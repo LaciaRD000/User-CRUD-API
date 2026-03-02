d1533c808f88e4e2b60d89fa671ba775aea7d99b
LaciaRD000 <radonxenon001@gmail.com>
2026-03-02 21:59:06 +0900

    update: passwordのvalidationを追加

--- src/validation.rs
+++ src/validation.rs
@@ -1,7 +1,7 @@
 pub fn validate_username(username: &str) -> Result<(), String> {
     if username.is_empty() {
         Err("username is empty".to_string())
-    } else if username.len() > 32 {
+    } else if username.chars().count() > 32 {
         Err("username is too long".to_string())
     } else {
         Ok(())
@@ -13,6 +13,18 @@ pub fn validate_email(email: &str) -> Result<(), String> {
         Err("email is empty".to_string())
     } else if !email.contains("@") {
         Err("email is invalid".to_string())
+    } else {
+        Ok(())
+    }
+}
+
+pub fn validate_password(password: &str) -> Result<(), String> {
+    if password.is_empty() {
+        Err("password is empty".to_string())
+    } else if password.chars().count() < 8 {
+        Err("password is too short".to_string())
+    } else if password.len() > 72 {
+        Err("password is too long".to_string())
     } else {
         Ok(())
     }
